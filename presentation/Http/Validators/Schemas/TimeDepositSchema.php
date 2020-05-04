a2f63279d56f92cafcc78417eff9bede965f8b4a
cristianvena18 <cristiandamianvena@gmail.com>
2020-05-04 20:20:04 -0300

    Update TimeDepositSchema.php

--- presentation/Http/Validators/Schemas/TimeDepositSchema.php
+++ presentation/Http/Validators/Schemas/TimeDepositSchema.php
@@ -10,7 +10,7 @@ class TimeDepositSchema
         return [
             'name' => 'bail|required|alpha|min:4|max:16',
             'surname' => 'bail|required|alpha|min:4|max:16',
-            'mount' => 'bail|required|numeric',
+            'mount' => 'bail|required|numeric|min:1000',
             'days' => 'bail|required|integer|min:30',
             //'compound' => 'bail|boolean|nullable'
         ];
